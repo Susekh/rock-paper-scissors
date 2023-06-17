eb371d93fa4a0e3bc2d634e0251dab0bbf3ae2c1
Subh <subhranshukhilar@outlook.com>
2023-06-18 00:33:10 +0530

    Added functions for choices and round of gameplay

--- new-script.js
+++ new-script.js
@@ -1,15 +1,76 @@
-let userChoice =0;
+let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-Score");
+let computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-rock_div.addEventListener('click', function(){
-    console.log("you clicked on rock")
+function getComputerChoice(){
+    const choices = ['r', 'p', 's'];
+    const randomeNumber = Math.floor(Math.random()*3);
+    return choices[randomeNumber];
+
 }
-)
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = userChoice + " beats " + computerChoice + " you win ";
+}
+function lose(userChoice, computerChoice){
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = computerChoice + " beats " + userChoice + " you lost ";
+}
+
+function draw() {
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = " draw ";
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw();
+            break;    
+    }
+}
+
+game();
+
+
+function main () {
+    rock_div.addEventListener('click', function(){
+        game("r");
+    })
+    paper_div.addEventListener('click', function(){
+        game("p");
+    })
+    scissors_div.addEventListener('click', function(){
+        game("s");
+    })
+}
+
+main();
+
+
 
