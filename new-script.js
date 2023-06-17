a481bcd84c98983e9e8a4030fe11ded04e90e6eb
Subh <subhranshukhilar@outlook.com>
2023-06-17 16:14:17 +0530

    Defined all the variables

---
+++ new-script.js
@@ -0,0 +1,15 @@
+let userChoice =0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-Score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+rock_div.addEventListener('click', function(){
+    console.log("you clicked on rock")
+}
+)
+
